3104a7dba1369aa5c35628f9bbd5fd3d692130ca
ran232003 <45311381+ran232003@users.noreply.github.com>
2022-04-03 08:18:54 +0300

    update

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AddQuote from './pages/AddQuote';
 import { Provider } from 'react-redux';
 import store from './store';
 import AllQuotes from './pages/AllQuotes';
+import QuotePage from './pages/QuotePage';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
 
       </Route>
       <Route exact  path = "/quotes" element = {<AllQuotes/>}>
+
+      </Route>
+      <Route exact  path = "/quotes/qute-page/:id" element = {<QuotePage/>}>
 
       </Route>
 
--- src/components/QuoteItem.js
+++ src/components/QuoteItem.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import "./QuoteItem.css";
 const QuoteItem = (props)=>{
     const {id,name,quote} = props;
     return(
+        <Link to={`qute-page/${id}`} style={{ textDecoration: 'none',color:'black' }}>
         <div className= 'quote'>
-            <p>{quote}</p>
+            <div className = "myQuote">
+            <p>"{quote}"</p>
+            </div>
+            <div className = "myName">
             <p>{name}</p>
+            </div>
         </div>
+        </Link>
     )
 }
 export default QuoteItem;
