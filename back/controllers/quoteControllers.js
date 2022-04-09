751fe3be7601e6280c272139f4724f77ce4ef17d
ran232003 <45311381+ran232003@users.noreply.github.com>
2022-04-09 20:56:30 +0300

    update api call

--- back/controllers/quoteControllers.js
+++ back/controllers/quoteControllers.js
@@ -7,23 +7,19 @@ app.use(bodyParser.json());
 const addQuote = (req,res,next)=>{
     console.log("start");
     console.log(req.body)
+    const {name,id,quote} = req.body;
     const errors = validationResult(req);
     if(!errors.isEmpty()){
        
         console.log("error")
         return res.json({msg:"error"})
     }
-    else{
-        console.log("mid");
-    }
+   
     console.log("add");
     return res.json({msg:"ok"})
 }
 
 
-const validate = (methodName)=>{
-
-}
 
 module.exports = {
     addQuote:addQuote
